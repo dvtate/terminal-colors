26964cabcaecea1b5fb1c88c607cf2bfe5878d9b
Dustin Van Tate Testa <toast27@gmail.com>
2016-11-13 22:47:16 -0500

    more features, almost done :)

--- example.cpp
+++ example.cpp
@@ -3,8 +3,8 @@
 
 int main(){
 
-	color_printf("rgb(255,0,255)", "hello world!\n");
-	color_printf("#FF00ff", "hello world!\n");
+	color_printf("rgb(255,0,255)", "hello world! %d\n", 666);
+	color_printf("#FF00ff", "hello world!\n%s\n", "printf in color! ");
 	color_printf("#f0f", "hello world!\n");
 	color_printf("magenta", "hello world!\n");
 	color_printf(255, 0, 255, "hello world!\n");
@@ -14,4 +14,16 @@ int main(){
 	color.g = 0;
 	color_printf(color, "hello world!\n");
 
+	setBgColor("#0ff");
+	setFgColor("#800");
+	printf("\nHello, I should be cyan...");
+	setBgColor("#0f0");
+	printf("derp...derp...derp\n");
+
+	resetASCII();
+	setEffect(TERM_CLR_BLINK);
+	printf("resetASCII() resets all terminal colors and effects.\n");
+
+
+	resetASCII(); // this should be at the end of all programs...
 }
